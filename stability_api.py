b9c167c0d0da559dce3e9719d06f26f42ce0b8ee
Aashish <aashishkumar7814@gmail.com>
2024-10-21 13:23:25 +0530

    upsacel image wot the  fast

--- stability_api.py
+++ stability_api.py
@@ -5,10 +5,10 @@ import os
 load_dotenv()
 stability_api_key = os.environ.get('STABILITY_API_KEY')
 
-# Function to upscale creative
-def upscale_creative(image):
+# Function to upscale fast
+def upscale_fast(image):
     response = requests.post(
-        "https://api.stability.ai/v2beta/stable-image/upscale/cretive",
+        "https://api.stability.ai/v2beta/stable-image/upscale/fast",
         headers={
             "authorization": f"Bearer {stability_api_key}",
             "accept": "image/*"
@@ -16,17 +16,16 @@ def upscale_creative(image):
         files={
             "image": open(image, "rb")
         },
-        data={
-            "prompt": "cute fluffy white kitten floating in space, pastel colors",
-            "output_format": "png",
-        },
+         data = {
+            "prompt": "make the  image  more  clear",
+            "output_format": 'png',
+            "creativity": 0.50,
+        }
+
     )
 
     # Check if the request was successful
     if response.status_code == 200:
-        # print("resposne.joson----->>>", response.json())
-        # print('response.content---->>>',response.content)
-
         # Save the image data to a local file
         with open('images\\blur_upscale_res1.png', 'wb') as file:
             file.write(response.content)
@@ -35,5 +34,58 @@ def upscale_creative(image):
         print(f"Failed to upscale image. Status code: {response.status_code}")
         print("Error:", response.text)
 
-# Call the function to upscale creative
-upscale_creative("images\\blur_upscale_test1.png")
+# Call the function to upscale fast
+# upscale_fast("blur_upscale_test1.png")
+
+
+
+
+# function to  cretive upscale
+def upscale_creative(image):
+    response = requests.post(
+        "https://api.stability.ai/v2beta/stable-image/upscale/fast",
+        headers={
+            "authorization": f"Bearer {stability_api_key}",
+            "accept": "image/*"
+        },
+        files={
+            "image": open(image, "rb")
+        },
+         data = {
+            "prompt": "make the  image  more  clear",
+            "output_format": 'png',
+            "creativity": 0.50,
+        }
+
+    )
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Save the image data to a local file
+        with open('images\\blur_upscale_res1.png', 'wb') as file:
+            file.write(response.content)
+        print("Image saved successfully.")
+    else:
+        print(f"Failed to upscale image. Status code: {response.status_code}")
+        print("Error:", response.text)
+
+# Call the function to upscale fast
+upscale_creative("blur_upscale_test1.png")
+
+# function fro  image to  image upscale
+# def image_to_image_upscale(image_data):
+#     response = requests.post(
+#     # https://api.stability.ai/v2beta/stable-image/upscale/cretive",
+#     # f"{API_HOST}/v1/generation/{UPSCALE_ENGINE_ID}/image-to-image/upscale",
+#     f"https://api.stability.ai/v1/generation/v2beta/image-to-image/upscale",
+#     headers={
+#         "Accept": "image/png",
+#         "Authorization": f"Bearer {stability_api_key}",
+#     },
+#     files={"image": ("upscale.png", image_data, "image/png")},
+#     )
+#     if response.status_code != 200:
+#         raise Exception("Non-200 response: " + str(response.text))
+
+
+# image_to_image_upscale(image_data)
