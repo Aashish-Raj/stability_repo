3f77bffd20ff98690147c590eed4360e3c4e2900
Aashish <aashishkumar7814@gmail.com>
2024-10-21 14:59:56 +0530

    crete the image by the creativ  api and  make the fetch the  data from the  the  api

--- stability_api.py
+++ stability_api.py
@@ -27,7 +27,7 @@ def upscale_fast(image):
     # Check if the request was successful
     if response.status_code == 200:
         # Save the image data to a local file
-        with open('images\\blur_upscale_res1.png', 'wb') as file:
+        with open('final_res\\blur_upscale_res1.png', 'wb') as file:
             file.write(response.content)
         print("Image saved successfully.")
     else:
@@ -35,12 +35,12 @@ def upscale_fast(image):
         print("Error:", response.text)
 
 # Call the function to upscale fast
-# upscale_fast("blur_upscale_test1.png")
+# upscale_fast("blur_img.png")
 
 
 
 
-# function to  cretive upscale
+# function to  conservative upscale
 def upscale_conservative(image):
     response = requests.post(
         f"https://api.stability.ai/v2beta/stable-image/upscale/conservative",
@@ -62,7 +62,7 @@ def upscale_conservative(image):
     # Check if the request was successful
     if response.status_code == 200:
         # Save the image data to a local file
-        with open('images\\blur_upscale_conservative_res1.png', 'wb') as file:
+        with open('final_res\\blur_upscale_conservative_res1.png', 'wb') as file:
             file.write(response.content)
         print("Image saved successfully.")
     else:
@@ -70,22 +70,66 @@ def upscale_conservative(image):
         print("Error:", response.text)
 
 # Call the function to upscale fast
-upscale_conservative("blur_upscale_test1.png")
+# upscale_conservative("blur_img.png")
 
-# function fro  image to  image upscale
-# def image_to_image_upscale(image_data):
-#     response = requests.post(
-#     # https://api.stability.ai/v2beta/stable-image/upscale/cretive",
-#     # f"{API_HOST}/v1/generation/{UPSCALE_ENGINE_ID}/image-to-image/upscale",
-#     f"https://api.stability.ai/v1/generation/v2beta/image-to-image/upscale",
-#     headers={
-#         "Accept": "image/png",
-#         "Authorization": f"Bearer {stability_api_key}",
-#     },
-#     files={"image": ("upscale.png", image_data, "image/png")},
-#     )
-#     if response.status_code != 200:
-#         raise Exception("Non-200 response: " + str(response.text))
+# function to  cretive upscale
+def upscale_creative(image):
+    response = requests.post(
+        f"https://api.stability.ai/v2beta/stable-image/upscale/creative",
+        headers={
+            "authorization": f"Bearer {stability_api_key}",
+            "accept": "image/*"
+        },
+        files={
+            "image": open(image, "rb"),
+        },
+         data = {
+            "prompt": "make the  image  more  clear",
+            "output_format": 'png',
+        }
 
+    )
+    
+    print("Generation ID:", response.json().get('id'))
+    return response.json().get('id')
 
-# image_to_image_upscale(image_data)
+    # # Check if the request was successful
+    # if response.status_code == 200:
+        
+    #     print("Generation ID:", response.json().get('id'))
+    #     # Save the image data to a local file
+    #     # with open('images\\blur_upscale_creative_res1.png', 'wb') as file:
+    #     #     file.write(response.content)
+    #     # print("Image saved successfully.")
+    # else:
+    #     print(f"Failed to upscale image. Status code: {response.status_code}")
+    #     print("Error:", response.text)
+
+# Call the function to upscale fast
+# id=upscale_creative("blur_img.png")
+# print('id--->>',id)
+id="4cb93251d974ed0dc6d652d8a7a53d14cbef174ae4662a017d53d7b6101542da"
+
+# fetcht the  image wwoth the   genertion id
+def get_image_fromId(id):
+    response = requests.request(
+    "GET",
+    f"https://api.stability.ai/v2beta/stable-image/upscale/creative/result/{id}",
+    headers={
+        'accept': "image/*",  # Use 'application/json' to receive base64 encoded JSON
+        'authorization': f"Bearer {stability_api_key}"
+    },
+)
+
+    if response.status_code == 202:
+        print("Generation in-progress, try again in 10 seconds.")
+    elif response.status_code == 200:
+        print("Generation complete!")
+        with open("final_res\\blur_upscale_creative_res1.png", 'wb') as file:
+            file.write(response.content)
+    else:
+        raise Exception(str(response.json()))
+    
+# call the  funtion to get the image
+# get_image_fromId("fdca9f89a8458c37d15674c4235a4fbd87d78e2f8bb21ff84f659bbb5195f881")
+get_image_fromId(id)
